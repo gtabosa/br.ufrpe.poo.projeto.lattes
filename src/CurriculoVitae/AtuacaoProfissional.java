2d0de6ed69350ba72fceb4afb8cc9d377853f0b8
Gedenilton Henrique <g.h.rocha21@hotmail.com>
2019-12-02 11:54:48 -0300

    simplificando as classes

--- src/CurriculoVitae/AtuacaoProfissional.java
+++ src/CurriculoVitae/AtuacaoProfissional.java
@@ -1,36 +1,7 @@
 package CurriculoVitae;
 
-public class AtuacaoProfissional {
+public interface AtuacaoProfissional {
 	
-	private int codInstituicao;
-	private String nomeInstituicao;
-	private int sequenciaAtividade;
-	private int sequenciaImportancia;
-	public int getCodInstituicao() {
-		return codInstituicao;
-	}
-	public void setCodInstituicao(int codInstituicao) {
-		this.codInstituicao = codInstituicao;
-	}
-	public String getNomeInstituicao() {
-		return nomeInstituicao;
-	}
-	public void setNomeInstituicao(String nomeInstituicao) {
-		this.nomeInstituicao = nomeInstituicao;
-	}
-	public int getSequenciaAtividade() {
-		return sequenciaAtividade;
-	}
-	public void setSequenciaAtividade(int sequenciaAtividade) {
-		this.sequenciaAtividade = sequenciaAtividade;
-	}
-	public int getSequenciaImportancia() {
-		return sequenciaImportancia;
-	}
-	public void setSequenciaImportancia(int sequenciaImportancia) {
-		this.sequenciaImportancia = sequenciaImportancia;
-	}
-	
-	
-	
+	void vinculoInstitucional();
+
 }
--- src/CurriculoVitae/FormacaoComplementarExtensaoUniversitaria.java
+++ src/CurriculoVitae/FormacaoComplementarExtensaoUniversitaria.java
@@ -3,175 +3,60 @@ package CurriculoVitae;
 import java.util.Date;
 
 public class FormacaoComplementarExtensaoUniversitaria {
-	private int sequenciaFormacao;
-	private int nivel;
-	private int cargaHoraria;
-	private int codInstituicao;
-	private String nomeInstituicao;
-	private int codOrgao;
-	private String nomeOrgao;
-	private int codCurso;
-	private String nomeCurso;
-	private String statusCurso;
-	private Date anoInicio;
-	private Date anoConclusao;
-	private String nomeCurso_EN;
-	
-	/**
-	 * @return the sequenciaFormacao
-	 */
-	public int getSequenciaFormacao() {
-		return sequenciaFormacao;
-	}
-	/**
-	 * @param sequenciaFormacao the sequenciaFormacao to set
-	 */
-	public void setSequenciaFormacao(int sequenciaFormacao) {
-		this.sequenciaFormacao = sequenciaFormacao;
-	}
-	/**
-	 * @return the nivel
-	 */
-	public int getNivel() {
-		return nivel;
-	}
-	/**
-	 * @param nivel the nivel to set
-	 */
-	public void setNivel(int nivel) {
-		this.nivel = nivel;
-	}
-	/**
-	 * @return the cargaHoraria
-	 */
-	public int getCargaHoraria() {
-		return cargaHoraria;
-	}
-	/**
-	 * @param cargaHoraria the cargaHoraria to set
-	 */
-	public void setCargaHoraria(int cargaHoraria) {
-		this.cargaHoraria = cargaHoraria;
-	}
-	/**
-	 * @return the codInstituicao
-	 */
-	public int getCodInstituicao() {
-		return codInstituicao;
-	}
-	/**
-	 * @param codInstituicao the codInstituicao to set
-	 */
-	public void setCodInstituicao(int codInstituicao) {
-		this.codInstituicao = codInstituicao;
-	}
-	/**
-	 * @return the nomeInstituicao
-	 */
-	public String getNomeInstituicao() {
-		return nomeInstituicao;
-	}
-	/**
-	 * @param nomeInstituicao the nomeInstituicao to set
-	 */
-	public void setNomeInstituicao(String nomeInstituicao) {
-		this.nomeInstituicao = nomeInstituicao;
-	}
-	/**
-	 * @return the codOrgao
-	 */
-	public int getCodOrgao() {
-		return codOrgao;
-	}
-	/**
-	 * @param codOrgao the codOrgao to set
-	 */
-	public void setCodOrgao(int codOrgao) {
-		this.codOrgao = codOrgao;
-	}
-	/**
-	 * @return the nomeOrgao
-	 */
-	public String getNomeOrgao() {
-		return nomeOrgao;
-	}
-	/**
-	 * @param nomeOrgao the nomeOrgao to set
-	 */
-	public void setNomeOrgao(String nomeOrgao) {
-		this.nomeOrgao = nomeOrgao;
-	}
-	/**
-	 * @return the codCurso
-	 */
-	public int getCodCurso() {
-		return codCurso;
-	}
-	/**
-	 * @param codCurso the codCurso to set
-	 */
-	public void setCodCurso(int codCurso) {
-		this.codCurso = codCurso;
-	}
-	/**
-	 * @return the nomeCurso
-	 */
-	public String getNomeCurso() {
-		return nomeCurso;
-	}
-	/**
-	 * @param nomeCurso the nomeCurso to set
-	 */
-	public void setNomeCurso(String nomeCurso) {
-		this.nomeCurso = nomeCurso;
-	}
-	/**
-	 * @return the statusCurso
-	 */
-	public String getStatusCurso() {
-		return statusCurso;
-	}
-	/**
-	 * @param statusCurso the statusCurso to set
-	 */
-	public void setStatusCurso(String statusCurso) {
-		this.statusCurso = statusCurso;
-	}
-	/**
-	 * @return the anoInicio
-	 */
-	public Date getAnoInicio() {
+
+	private String anoInicio;
+	private Date anoFim;
+	private Date descricao;
+	private String universidade;
+	private String titulo;
+	private String local;
+
+	public String getAnoInicio() {
 		return anoInicio;
 	}
-	/**
-	 * @param anoInicio the anoInicio to set
-	 */
-	public void setAnoInicio(Date anoInicio) {
+
+	public void setAnoInicio(String anoInicio) {
 		this.anoInicio = anoInicio;
 	}
-	/**
-	 * @return the anoConclusao
-	 */
-	public Date getAnoConclusao() {
-		return anoConclusao;
+
+	public Date getAnoFim() {
+		return anoFim;
 	}
-	/**
-	 * @param anoConclusao the anoConclusao to set
-	 */
-	public void setAnoConclusao(Date anoConclusao) {
-		this.anoConclusao = anoConclusao;
+
+	public void setAnoFim(Date anoFim) {
+		this.anoFim = anoFim;
 	}
-	/**
-	 * @return the nomeCurso_EN
-	 */
-	public String getNomeCurso_EN() {
-		return nomeCurso_EN;
+
+	public Date getDescricao() {
+		return descricao;
 	}
-	/**
-	 * @param nomeCurso_EN the nomeCurso_EN to set
-	 */
-	public void setNomeCurso_EN(String nomeCurso_EN) {
-		this.nomeCurso_EN = nomeCurso_EN;
+
+	public void setDescricao(Date descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getUniversidade() {
+		return universidade;
+	}
+
+	public void setUniversidade(String universidade) {
+		this.universidade = universidade;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+	
+	public void setLocal(String local) {
+		this.local = local;
 	}
 	
 }
